1bfa72bb3b9ff702d4edb320c3005f1f8cf1a1c3
victor-devv <vheecthurikuomola@gmail.com>
2022-05-29 15:51:39 +0100

    Creates endpoint to find book by id

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	//"errors"
@@ -24,13 +25,34 @@ func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
+func bookById(c *gin.Context) {
+	id := c.Param("id")
+	book, err := getBookByID(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found!"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func getBookByID(id string) (*book, error) {
+	for i, b := range books {
+		if b.ID == id {
+			return &books[i], nil
+		}
+	}
+
+	return nil, errors.New("Book not found")
+}
+
 func createBook(c *gin.Context) {
 	//get data of book to be created
 	var newBook book
 
 	//Bind json from payload to book struct
 	if err := c.BindJSON(&newBook); err != nil {
-		//return error response
 		return
 	}
 
@@ -42,6 +64,7 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/books", getBooks)
+	router.GET("/books/:id", bookById)
 	router.POST("/books", createBook)
 	router.Run("localhost:8080")
 }
