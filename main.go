ed51eb42a78fed0f283e358993b99b04e3a2c32d
victor-devv <vheecthurikuomola@gmail.com>
2022-05-29 15:43:04 +0100

    Creates endpoint to add a book

--- main.go
+++ main.go
@@ -19,14 +19,29 @@ var books = []book{
 	{ID: "3", Title: "Book 3", Author: "Cyril Autumn", Quantity: 6},
 }
 
-//*gin.Context is all of the info about a request and allows you to return a response
+//*gin.Context has all of the info (query params, payload, etc) about a request and allows you to return a response
 func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
+}
+
+func createBook(c *gin.Context) {
+	//get data of book to be created
+	var newBook book
+
+	//Bind json from payload to book struct
+	if err := c.BindJSON(&newBook); err != nil {
+		//return error response
+		return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
 }
 
 func main() {
 	router := gin.Default()
 
 	router.GET("/books", getBooks)
+	router.POST("/books", createBook)
 	router.Run("localhost:8080")
 }
