416cd81fcd910f133e37464cfa1242e4dc2ae0a6
victor-devv <vheecthurikuomola@gmail.com>
2022-05-29 15:32:56 +0100

    Creates endpoint to get all books

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"errors"
+	//"errors"
 )
 
 type book struct {
@@ -17,4 +17,16 @@ var books = []book{
 	{ID: "1", Title: "Book 1", Author: "John Doe", Quantity: 2},
 	{ID: "2", Title: "Book 2", Author: "Jane Doe", Quantity: 5},
 	{ID: "3", Title: "Book 3", Author: "Cyril Autumn", Quantity: 6},
+}
+
+//*gin.Context is all of the info about a request and allows you to return a response
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/books", getBooks)
+	router.Run("localhost:8080")
 }
